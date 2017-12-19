db7cc48a8c5790a5476d47db714688b62505b43b
Jeffrey Powell <jpowell@lhediscovery.com>
2017-12-18 20:31:11 -0800

    reformatting solution 13

--- AdventOfCode13-1/Program.cs
+++ AdventOfCode13-1/Program.cs
@@ -5,16 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace AdventOfCode13_1
-{
-    class Program
-    {
+namespace AdventOfCode13_1 {
+    class Program {
         static int CurrentTime = 0;
         static int TripSeverity = 0;
         static bool Caught = false;
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             //Read the input.
             string input = @"C:\Users\jpowell\source\repos\AdventOfCode2017\AdventOfCode13-1\myInput.txt";
             List<int[]> firewallData = ReadInput(input);
@@ -23,8 +20,7 @@ namespace AdventOfCode13_1
             List<Firewall> myFirewalls = new List<Firewall>();
 
             //Initialize the firewalls and add them to the list.
-            foreach(int[] firewall in firewallData)
-            {
+            foreach (int[] firewall in firewallData) {
                 myFirewalls.Add(new Firewall(firewall[0], firewall[1]));
             }
 
@@ -32,52 +28,44 @@ namespace AdventOfCode13_1
             CurrentTime = 0;
             //Max time should be equal to the depth of the last firewall.
             int maxTime = 0;
-            foreach (Firewall firewall in myFirewalls)
-            {
-               if(firewall.Depth > maxTime)
-                {
+            foreach (Firewall firewall in myFirewalls) {
+                if (firewall.Depth > maxTime) {
                     maxTime = firewall.Depth;
                 }
             }
 
             //Move forward until you reach maxTime
-            while(CurrentTime <= maxTime)
-            {
-                MoveForward(myFirewalls,0);
+            while (CurrentTime <= maxTime) {
+                MoveForward(myFirewalls, 0);
             }
 
 
-            
+
             Console.WriteLine("Finished! 0 ps departure Trip Severity: " + TripSeverity.ToString());
             Console.WriteLine("Caught: " + Caught.ToString());
             Console.ReadKey();
 
             int offset = 1;
-            while(true)
-            {
+            while (true) {
                 //Reset certain variables and apply an offset.
                 CurrentTime = 0 + offset;
                 int offsetMaxTime = maxTime + offset;
                 TripSeverity = 0;
                 Caught = false;
 
-                while (CurrentTime <= offsetMaxTime)
-                {
+                while (CurrentTime <= offsetMaxTime) {
                     MoveForward(myFirewalls, offset);
                     //Early break to save time.
-                    if(Caught)
-                    {
+                    if (Caught) {
                         break;
                     }
                 }
 
                 //Provide a break condition.
-                if(!Caught)
-                {
+                if (!Caught) {
                     break;
                 }
-                else
-                {
+                else {
                     offset++;
                 }
             }
@@ -86,17 +74,14 @@ namespace AdventOfCode13_1
             Console.ReadKey();
         }
 
-        static void MoveForward(List<Firewall> myFirewalls, int offset)
-        {
+        static void MoveForward(List<Firewall> myFirewalls, int offset) {
             //Using current time, identify if a firewall exists on that node.
             Firewall currentFirewall = myFirewalls.FirstOrDefault(x => x.Depth == CurrentTime - offset);
 
             //if we found one, check if it's at the top of its cycle (i.e. Location returns 1)
-            if(currentFirewall!=null)
-            {
+            if (currentFirewall != null) {
                 //If it is at the top of its cycle, add its Severity to the static tripSeverity.
-                if(currentFirewall.Catches(CurrentTime))
-                {
+                if (currentFirewall.Catches(CurrentTime)) {
                     Caught = true;
                     TripSeverity += currentFirewall.Severity;
                 }
@@ -106,20 +91,17 @@ namespace AdventOfCode13_1
             CurrentTime++;
         }
 
-        static List<int[]>ReadInput(string input)
-        {
+        static List<int[]> ReadInput(string input) {
             List<int[]> result = new List<int[]>();
             //Reads input from the provided textfile.
-            using (StreamReader sr = new StreamReader(input))
-            {
+            using (StreamReader sr = new StreamReader(input)) {
                 //Returns data as a list of arrays. The data is provided in formation DEPTH: RANGE where depth and range are both integers.
-                while(!sr.EndOfStream)
-                {
+                while (!sr.EndOfStream) {
                     string line = sr.ReadLine();
                     string[] lineArray = line.Split(':');
                     int depth = Int32.Parse(lineArray[0]);
                     int range = Int32.Parse(lineArray[1].Trim());
-                    int[] newEntry = new int[] { depth,range };
+                    int[] newEntry = new int[] { depth, range };
                     result.Add(newEntry);
                 }
             }
@@ -129,59 +111,49 @@ namespace AdventOfCode13_1
         }
     }
 
-    class Firewall
-    {
+    class Firewall {
         //Firewalls have a range and a depth. This will be set on creation.
         private int range;
         private int depth;
 
         public int Range { get { return range; } }
         public int Depth { get { return depth; } }
-        
+
         //The severity of a firewall is equal to its range times its depth.
-        public int Severity
-        {
-            get
-            {
+        public int Severity {
+            get {
                 return range * depth;
             }
         }
 
         //declare firewalls with their properties set.
-        public Firewall(int depth, int range)
-        {
+        public Firewall(int depth, int range) {
             this.range = range;
             this.depth = depth;
         }
 
         //returns the current location of the firewall at any given time.
-        public int Location(int timeElapsed)
-        {
+        public int Location(int timeElapsed) {
             //calculate a full cycle, then figure out where in the cycle we are.
             int cycleLength = 2 * (range - 1);
             int counterValue = timeElapsed;
-            while(counterValue >= cycleLength)
-            {
+            while (counterValue >= cycleLength) {
                 counterValue -= cycleLength;
             }
 
             //when the counterValue is larger than half the cycle, we do a calculation to find its actual location(1 is the top level, range is the bottom level)
-            if(counterValue > cycleLength/2)
-            {
-                return(cycleLength - counterValue);
+            if (counterValue > cycleLength / 2) {
+                return (cycleLength - counterValue);
             }
-            else
-            {
+            else {
                 return counterValue;
             }
         }
 
         //returns whether it will catch you at a given time
-        public bool Catches(int timeElapsed)
-        {
+        public bool Catches(int timeElapsed) {
             int cycleLength = 2 * (range - 1);
-            if (timeElapsed % cycleLength == 0)
-            {
+            if (timeElapsed % cycleLength == 0) {
                 return true;
             }
             else return false;
