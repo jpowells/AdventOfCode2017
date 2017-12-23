0af8f0f98691dd4ad6c39336675e4510a8c9ac52
lhejpowell <jpowell@lhediscovery.com>
2017-12-22 22:43:20 -0800

    start 18

---
+++ AdventOfCode18-1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode18_1 {
+
+
+
+    class Program {
+        static int CurrentPos = 0;
+        static bool BreakLoop = false;
+        static Dictionary<char, int> MyNotes = new Dictionary<char,int>();
+
+        static void Main(string[] args) {
+            //Read the input.
+            string myInput = @"C:\Users\ZomB\Source\Repos\AdventOfCode2017\AdventOfCode18-1\Input.txt";
+            string[] commands = ReadCommandsFromFile(myInput);
+            
+            for(; CurrentPos < commands.Length; CurrentPos++) {
+                ParseCommand(commands[CurrentPos]);
+                if(BreakLoop) {
+                    break;
+                }
+            }
+        }
+
+        static void ParseCommand(string command) {
+
+        }
+
+        //Reads a list of commands from a file into a string array.
+        static string[] ReadCommandsFromFile(string filepath) {
+            List<string> commands = new List<string>();
+            using (StreamReader sr = new StreamReader(filepath)) {
+                while (!sr.EndOfStream) {
+                    commands.Add(sr.ReadLine());
+                }
+            }
+            return (commands.ToArray());
+        }
+    }
+}
