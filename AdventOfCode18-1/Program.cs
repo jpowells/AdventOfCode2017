bd61285d16c6a281f6bcbe05b7cf4664fb99544e
lhejpowell <jpowell@lhediscovery.com>
2017-12-25 17:20:52 -0800

    aoc18pt2

--- AdventOfCode18-1/Program.cs
+++ AdventOfCode18-1/Program.cs
@@ -8,6 +8,19 @@ using System.IO;
 namespace AdventOfCode18_1 {
 
 
+
+    class Program {
+        static int CurrentPos = 0;
+        static bool BreakLoop = false;
+        static Dictionary<char, int> MyNotes = new Dictionary<char,int>();
+        static Stack<int> NotesPlayed = new Stack<int>();
+
+        static void Main(string[] args) {
+            //Read the input.
+            string myInput = @"C:\Users\ZomB\Source\Repos\AdventOfCode2017\AdventOfCode18-1\Input.txt";
+            string[] commands = ReadCommandsFromFile(myInput);
+            
+            for(; CurrentPos < commands.Length; CurrentPos++) {
                 if(ParseCommand(commands[CurrentPos])) {
                     break;
                 }
