9b3b4aad10e835103d1f3b8a6d547a38cdd45997
lhejpowell <jpowell@lhediscovery.com>
2018-01-11 16:41:50 -0800

    Outline the beginning of day 19

---
+++ AdventOfCode19-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode19_1 {
+    class Program {
+        static void Main(string[] args) {
+            string inputPath = @"C:\Users\jpowell\source\repos\AdventOfCode2017\AdventOfCode19-1\Input.txt";
+            char[][] grid = ReadGridFromFile(inputPath);
+
+
+        }
+
+        static char[][] ReadGridFromFile(string input) {
+            List<string> Lines = new List<String>();
+            using(StreamReader sr = new StreamReader(input)) {
+                while (!sr.EndOfStream) {
+                    Lines.Add(sr.ReadLine());
+                }
+            }
+
+            char[][] result = new char[Lines.Count][];
+            for(int i = 0; i < Lines.Count; i++) {
+                result[i] = Lines[i].ToArray();
+            }
+
+            return result;
+        }
+    }
+}
